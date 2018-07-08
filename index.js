bcae6083d3bbe0b873124d41864f298f3fa74e0d
Joseph Parker <jparker010190@gmail.com>
2018-07-08 23:35:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ class BoardMember {
   releasePressStatement(){return "You will see great things from Scuber."}
   sayHi(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`}
 }
+class CEO extends BoardMember{
+  hireEmployee(){
+    return "Welcome Aboard!"
+  }
+}
